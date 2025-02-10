667004620cf8735a3a2a45ed45a51fe313ed624a
ChiwooKim <kcw0360@naver.com>
2025-02-10 18:03:10 +0900

    Feat: Create Project
    - 프로젝트 생성

---
+++ collabtodo-backend/src/main/java/com/collabtodo/collabtodo_backend/CollabtodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.collabtodo.collabtodo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabtodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabtodoBackendApplication.class, args);
+	}
+
+}
